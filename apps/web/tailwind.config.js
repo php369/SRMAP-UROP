8f9887f2921413dd77476e16a7335d6c969f8d85
Poojan Patel <patelpoojan308@gmail.com>
2025-12-05 11:53:08 +0530

    frontend fix

--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  ...require('@srm-portal/config/tailwind/base'),
   content: [
     './index.html',
-    './src/**/*.{js,ts,jsx,tsx}',
-    '../../packages/ui/src/**/*.{js,ts,jsx,tsx}'
-  ]
+    './src/**/*.{js,ts,jsx,tsx}'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
 };
